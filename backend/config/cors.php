c1fb4472f21b1763cd8d8fd0343e09b1c476e9b2
Gabriel Rodrigues da Silva <grs.ibr@gmail.com>
2026-03-23 03:30:50 -0300

    Fix: Configure CORS for production environment

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -16,19 +16,15 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
+    
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    
+    'supports_credentials' => true,
 
 ];
